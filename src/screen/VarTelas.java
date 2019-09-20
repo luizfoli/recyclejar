e58610377b0fcd5727b4bbfb054119551d58bb41
Luiz Oliveira <oliveira.lfo@gmail.com>
2019-09-20 17:36:12 -0300

    Removing

--- src/screen/VarTelas.java
+++ src/screen/VarTelas.java
@@ -2,32 +2,14 @@ package screen;
 
 public class VarTelas {
 
-	/**********************************************************/
-	/********************** START - GAME **********************/
+	private final String LOGO_GAME = "logo_game.png";
+	private final String TITLE_GAME = "Recicle!";
 
-	// Variaveis
-
-	private static final String _LOGO_GAME = "logo_game.png";
-	private final String _TITLE_GAME = "Recicle!";
-
-	// Getters
-
-	public String get_logoGame() {
-		return _LOGO_GAME;
+	public String getLogoGame() {
+		return LOGO_GAME;
 	}
 
-	public String get_titleGame() {
-		return _TITLE_GAME;
+	public String getTitleGame() {
+		return TITLE_GAME;
 	}
-
-	/**********************************************************/
-	/**********************************************************/
-
-	/********************** MENU - GAME **********************/
-
-	// Variaveis
-
-	// Getters
-
-	/*********************************************************/
 }
